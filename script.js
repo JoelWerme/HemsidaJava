d18d3d588effd6ab7162a83bcbcbc7e2d58cec6c
JoelWerme <joelwer3@gmail.com>
2021-03-06 18:54:02 +0100

    små ändringar

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let horizontal = 25;
 let vertical = 0;
 let foodX;
 let foodY;
-let gameOverMsg = 'Game Over. Refresh this page to start over.';
-let winMsg = 'Congrats, you have won! Refresh this page to start over.'
+let gameOverMsg = 'Game Over.';
+let winMsg = 'Congrats, you have won!'
 
 main();
 generate();
